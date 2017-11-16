cc3a6cbef5d0940526ad1a7f5e64e09acdbccea3
Christian Ted Ochoa <ochoatechmaster25@gmail.com>
2017-11-16 21:25:15 +0800

    Set up CustomizationManager

---
+++ application/customization/CustomizationManager.php
@@ -0,0 +1,30 @@
+<?php
+  class CustomizationManager{
+    public $currentConfigName;   // saves the current configuration name
+    public $colors;  // an object that stores the colors
+
+    // Load the respective configurations based on configName:
+    //  "configurations 1" for Ted's team;
+    //  "configurations 2" for Neil's team; or
+    //  null for default configurations.
+    public function SetTheme(string configName){
+
+    }
+
+    // Load the colors from the Colors1.php file in the selected configuration.
+    // Call this from within SetTheme method.
+    public function LoadColors(string configName){
+
+    }
+
+    // Call this from within SetTheme method.
+    public function LoadImages(string configName){
+
+    }
+
+    // Call this from within SetTheme method.
+    public function LoadLanguages(string configName){
+
+    }
+  }
+?>
---
+++ application/customization/configurations 1/Colors1.php
@@ -0,0 +1,8 @@
+<?php
+  class Colors1{
+    // Declare constants of colors. One will make those NAVBAR_BASE_COLOR stuff
+    // and the other will set values for them.
+    public const NAVBAR_BASE_COLOR = #e0e0e0;
+    public const NAVBAR_SHADOW_COLOR = #000000;
+  }
+?>
