f0f104393b5f66c5e371fea817cee57a905cc32a
ejmstark <ejmstark@gmail.com>
2017-11-20 21:08:49 +0800

    Update cLogin and cInitialize

--- application/controllers/cInitialize.php
+++ application/controllers/cInitialize.php
@@ -2,15 +2,20 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class CInitialize extends CI_Controller {
-
+	function __construct() {
+		parent::__construct();
+	 	$this->load->library('session');
+	 	$this->load->model('user/MEvent');
+	}
 
 	public function index()
 	{	
 		if ($this->session->userdata('userSession')) {
 			redirect('cLogin/viewDashBoard');
 		} else {
+			$data['events'] = $this->MEvent->getAllApprovedEvents();
 			$this->load->view('imports/vHeaderHomepage');
-			$this->load->view('vHomepage');
+			$this->load->view('vHomepage',$data);
 			$this->load->view('imports/vFooterHomepage');
 			// $this->load->view('vLogin');
 			//redirect('cInitialize');
