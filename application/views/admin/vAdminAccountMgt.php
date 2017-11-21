719774fb3e72b861f56df8b2278f3f8f3d9c6cfa
escoroiv <escoro.ivan@gmail.com>
2017-11-21 23:11:11 +0800

    sprint 1 tasks

--- application/views/admin/vAdminAccountMgt.php
+++ application/views/admin/vAdminAccountMgt.php
@@ -1,5 +1,8 @@
  <body class="">
 
+     <?php
+         foreach($ownAdminAccount as $updateAdmin) { }
+     ?>
     <header class="a-event-header sticky-top">
       <div class="header-content">
         <p class="text-center"><img class="sys-logo" src=<?php echo base_url('assets/jcAssets/pics/main-logo-prime.png')?> alt="Event system logo"></p>
@@ -18,7 +21,7 @@
                   <img id="user-pic" src=<?php echo base_url('assets/jcAssets/pics/user-2.png')?> alt="User picture">
                 </div>
                 <div class="col-sm-8 d-none d-sm-none d-md-block" >
-                  <h1><?php echo $this->session->userdata['userSession']->userFName; ?></h1>
+                  <h1><?php echo $updateAdmin->first_name; ?></h1>
                   <p class="user-role">Administrator</p>
                 </div>
               </div>
@@ -26,16 +29,16 @@
           </div>
 
           <div class="lower-sbar">
-            
+
             <li ><a href="<?php echo site_url();?>/admin/cAdmin">
               <p>
                <div class="d-block d-sm-block d-md-none">
                   <center> <i class="fa fa-list-alt" aria-hidden="true"></i> </center>
                 </div>
                <span class= "d-none d-sm-none d-md-inline">
-                  <i class="fa fa-list-alt" aria-hidden="true"></i> 
-                  Events 
-                </span> 
+                  <i class="fa fa-list-alt" aria-hidden="true"></i>
+                  Events
+                </span>
               </a></li></p>
             <li ><a href="<?php echo site_url();?>/admin/cAdmin/viewUserAccountMgt">
               <p>
@@ -90,6 +93,7 @@
             <div class="col-10">
               <h3><center>Admin Management<center></h3>
               <button class="btn btn-outline-primary" style="margin-bottom:25px;" type="button" name="button" data-toggle="modal" data-target="#addAdmin">Add Admin</button>
+              <button class="btn btn-outline-primary" style="margin-bottom:25px;" type="button" name="button" data-toggle="modal" data-target="#updateAccount">Update Account</button>
               <table class="table table-hover table-responsive table-sm">
                 <thead class="">
                     <tr>
@@ -103,12 +107,16 @@
                       <th>Date Created</th>
                       <th>Type</th>
                       <th>Status</th>
-                      <th>Action</th>
+                      <?php
+                     if($this->session->userdata['userSession']->userLevel == 'Superadmin'){
+                         echo"<th>Action</th>";
+                       }
+                     ?>
                     </tr>
                 </thead>
 
                   <tbody>
-                    <?php 
+                    <?php
                                 if($admin!=FALSE){
                                   foreach ($admin as $object) {
                           $num = ($object->contact_no != NULL)? $object->contact_no : "N/A";
@@ -123,29 +131,44 @@
                                 <td>".$num."</td>
                                 <td>".$object->date_account_created."</td>
                                 <td>".$object->user_type."</td>
-                                <td>".$object->user_status."</td>
-                                <td>";
-                              
-                              if ($this->session->userdata['userSession']->userID != $object->account_id){  
-                                if($object->user_type == "Admin"){
-                                  echo "<a  href='".site_url()."/admin/cAdmin/SuperAdmin/".$object->account_id."'>
-                                    <button  type='button' class='btn btn-warning'>Update Type</button></a>";
+                                <td>".$object->user_status."</td>";
+
+                                if($this->session->userdata['userSession']->userLevel == "Superadmin" && $object->user_status != "Deleted"){
+                                  echo"<td>";
+                                if ($this->session->userdata['userSession']->userID != $object->account_id){
+                                  if($object->user_type == "Admin"){
+                                    echo "<a  href='".site_url()."/admin/cAdmin/SuperAdmin/".$object->account_id."'>
+                                      <button  type='button' class='btn btn-warning'>Update Type</button></a>";
+                                  }else{
+                                      if($this->session->userdata['userSession']->userID == $object->upgraded_by){
+                                            echo "<a  href='".site_url()."/admin/cAdmin/Admin/".$object->account_id."'>
+                                             <button  type='button' class='btn btn-warning'>Update Type</button></a>";
+
+                                             echo "<a  href='".site_url()."/admin/cAdmin/Delete/".$object->account_id."/admin'>
+                                               <button  type='button' class='btn btn-danger'>Delete Account</button></a>";
+                                        }
+                                  }
+
+                                  if($this->session->userdata['userSession']->userSuperior != $object->account_id && $this->session->userdata['userSession']->userSuperior != $object->upgraded_by || $object->user_type == "Admin"){
+                                      if($object->user_status == "Active"){
+                                        echo "<a  href='".site_url()."/admin/cAdmin/Ban/".$object->account_id."/admin'>
+                                          <button  type='button' class='btn btn-primary'>Update Status</button></a>";
+                                      }else{
+                                        echo "<a  href='".site_url()."/admin/cAdmin/Unban/".$object->account_id."/admin'>
+                                          <button  type='button' class='btn btn-primary'>Update Status</button></a>";
+                                      }
+                                  }
+                                  if($object->user_type == "Admin"){
+                                      echo "<a  href='".site_url()."/admin/cAdmin/Delete/".$object->account_id."/admin'>
+                                        <button  type='button' class='btn btn-danger'>Delete Account</button></a>";
+                                  }
                                 }else{
-                                  echo "<a  href='".site_url()."/admin/cAdmin/Admin/".$object->account_id."'>
-                                    <button  type='button' class='btn btn-warning'>Update Type</button></a>";
+                                  //echo "Can't ban yourself.";
+                                  echo "<a  href='".site_url()."/admin/cAdmin/Delete/".$object->account_id."/admin'>
+                                    <button  type='button' class='btn btn-danger'>Delete Account</button></a>";
                                 }
-                                
-                                if($object->user_status == "Active"){
-                                  echo "<a  href='".site_url()."/admin/cAdmin/Ban/".$object->account_id."/admin'>
-                                    <button  type='button' class='btn btn-primary'>Update Status</button></a>";
-                                }else{
-                                  echo "<a  href='".site_url()."/admin/cAdmin/Unban/".$object->account_id."/admin'>
-                                    <button  type='button' class='btn btn-primary'>Update Status</button></a>";
-                                }
-                              }else{
-                                echo "Can't ban yourself.";
-                              }
-                              echo "</td></tr>";
+                                echo "</td></tr>";
+                            }
                         }
                                 }
                               ?>
@@ -177,7 +200,7 @@
 							</div>
 
 							<div class="modal-body form-horizontal ">
-                
+
                 <div class="form-group" >
                 	<label for="" class="col-8 control-label">First name:</label>
                 	<div class="col-8">
@@ -268,6 +291,103 @@
 				</div>
 			</div>
 		</div>
+
+        <div class="content">
+          <!-- Modal -->
+          <div id="updateAccount" class="modal fade" role="dialog">
+            <div class="modal-dialog modal-lg">
+
+              <!-- Modal content-->
+              <form class="form-horizontal" method="POST" action="<?php echo site_url()?>/admin/cAdmin/updateAdmin">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" style="color:#ffffff;">&times;</button>
+                    <h4 class="modal-title">Update Account: <span style="font-size: 18px;"><?php echo $updateAdmin->user_type;?></span></h4>
+                    <input class="form-control" type="text" name="uuserType" required="" value="<?php echo $updateAdmin->user_type; ?>" hidden>
+                  </div>
+
+                  <div class="modal-body form-horizontal ">
+
+                    <div class="form-group" >
+                      <label for="" class="col-8 control-label">First name:</label>
+                      <div class="col-8">
+                        <input class="form-control" type="text" name="ufname" required="" value="<?php echo $updateAdmin->first_name; ?>">
+                      </div>
+                    </div>
+
+                    <div class="form-group" >
+                      <label for="" class="col-8 control-label">Middle Initial:</label>
+                      <div class="col-8">
+                        <input class="form-control" type="text" name="uminame" required="" value="<?php echo $updateAdmin->middle_initial; ?>">
+                      </div>
+                    </div>
+
+                    <div class="form-group" >
+                      <label for="" class="col-8 control-label">Last name:</label>
+                      <div class="col-8">
+                        <input class="form-control" type="text" name="ulname" required="" value="<?php echo $updateAdmin->last_name; ?>">
+                      </div>
+                    </div>
+
+                    <div class="form-group" >
+                      <label for="" class="col-8 control-label">Email:</label>
+                      <div class="col-8">
+                        <input class="form-control" type="text" name="uemail" required="" value="<?php echo $updateAdmin->email; ?>">
+                      </div>
+                    </div>
+
+                    <div class="form-group" >
+                      <label for="" class="col-8 control-label">Birthdate:</label>
+                      <div class="col-8">
+                        <input class="form-control" type="date" name="ubdate" required="" value="<?php echo $updateAdmin->birthdate; ?>">
+                      </div>
+                    </div>
+
+                    <div class="form-group" >
+                      <label for="" class="col-8 control-label">Gender:</label>
+                      <div class="col-8">
+                      <select class="form-control" name="ugender" required=""> <br>
+                        <option value="Male" <?php if($updateAdmin->gender=='Male') {echo "selected=''";}?> >Male</option>
+                        <option value="Female" <?php if($updateAdmin->gender=='Female') {echo "selected=''";}?>>Female</option>
+                        <option value="Other" <?php if($updateAdmin->gender=='Other') {echo "selected=''";}?>>Other</option>
+                      </select>
+                      </div>
+                    </div>
+
+                    <div class="form-group" >
+                      <label for="" class="col-8 control-label">Contact no:</label>
+                      <div class="col-8">
+                        <input class="form-control" type="number" min="11" name="ucontact" required="" value="<?php echo $updateAdmin->contact_no; ?>">
+                      </div>
+                    </div>
+
+                    <br><br>
+
+                    <div class="form-group" >
+                      <label for="" class="col-8 control-label">Username:</label>
+                      <div class="col-8">
+                        <input class="form-control" type="text" name="uuname" required="" value="<?php echo $updateAdmin->user_name; ?>">
+                      </div>
+                    </div>
+
+                    <div class="form-group" >
+                      <label for="" class="col-8 control-label">Password:</label>
+                      <div class="col-8">
+                        <input class="form-control" type="password" name="upassword" required="" value="<?php echo $updateAdmin->password; ?>">
+                      </div>
+                    </div>
+
+                  </div>
+
+                  <div class="modal-footer">
+                    <button id="closeEditAccount" type="button" class="btn btn-danger" data-dismiss="modal" >Close</button>
+                    <input id="" class="btn btn-primary" type="submit"  name="action" value="Update">
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
 
   </body>
 </html>
