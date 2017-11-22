a6b477cd49e788287b839ed9c81dd6c517cb9c15
Coracolumn <justinediez23@gmail.com>
2017-11-22 21:37:10 +0800

    henlo
    added configImage class

--- application/customization/CustomizationManager.php
+++ application/customization/CustomizationManager.php
@@ -3,13 +3,15 @@
   class CustomizationManager{
     public $currentConfigName;   // saves the current configuration name
     public $colors;  // an object that stores the colors
+    public $images; //links to the images found in assets
 
     // Load the respective configurations based on configName:
     //  "configurations 1" for Ted's team;
     //  "configurations 2" for Neil's team; or
     //  null for default configurations.
     public function SetTheme(string configName){
-        LoadColors(configName);
+        configColor(configName);
+        configImage(configName);
     }
 
     // Load the colors from the Colors1.php file in the selected configuration.
@@ -20,7 +22,7 @@
 
     // Call this from within SetTheme method.
     public function configImage(string configName){
-
+        return $images = new Images1();
     }
 
     // Call this from within SetTheme method.
