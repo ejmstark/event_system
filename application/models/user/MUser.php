47ad137771782ac535d183fbede914176b85ad6f
algreg1998 <algreg17@gmail.com>
2017-12-12 20:47:17 +0800

    resolve conflict #2

--- application/models/user/MUser.php
+++ application/models/user/MUser.php
@@ -37,8 +37,8 @@
 		public function attemptLogin(){
 				//$hashPass=hash('sha512',$this->agentPassword);
 
-			//$query= $this->db->get_where($this::DB_TABLE,array('user_name'=>$this->user_name,'password'=>$this->user_password));
-			$query= $this->db->get_where($this::DB_TABLE,array("user_name like binary"=>$this->user_name,"password like binary"=>$this->user_password));
+			$query= $this->db->get_where($this::DB_TABLE,array('user_name'=>$this->user_name,'password'=>$this->user_password));
+			// $query= $this->db->get_where($this::DB_TABLE,array("user_name like binary"=>$this->user_name,"password like binary"=>$this->user_password));
 			if($query -> num_rows() == 1){
 			    return $query->result();
 			}else{
