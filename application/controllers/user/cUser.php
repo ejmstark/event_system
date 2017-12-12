2536fbd67eedf72137944d07da8c4df32582c303
algreg1998 <algreg17@gmail.com>
2017-12-12 20:39:45 +0800

    resolve conflicts

--- application/controllers/user/cUser.php
+++ application/controllers/user/cUser.php
@@ -9,29 +9,32 @@ class cUser extends CI_Controller {
       $this->load->model('user/MUser');
       $this->load->model('user/MEvent');
       $this->load->model('MCardLoad');
+			$this->load->model('MAnnouncement'); //admin module functionality
       $this->load->library('session');
-     	
+      $this->data = null;
   	}
 
   	public function redeemCode(){
-		
-		$code = $this->input->post('ccode');
-		$card = $this->MCardLoad->read_where( array('card_code' => $code ));
-		if($card != 0){
-			$card = json_decode(json_encode($card));
-			echo $card[0]->card_amount;
-			$u =  $this->MUser->read($this->session->userdata['userSession']->userID);
-			$cardNew = $u[0]->load_amt + $card[0]->card_amount;
-			
-			$res = $this->MUser->update($this->session->userdata["userSession"]->userID,array('load_amt'=>$cardNew));
 
-			if($res){
-				$res1 = $this->MCardLoad->update($code, array('card_active' => '0'));
-				redirect("event/cEvent/viewEvents");
+		$code = $this->input->post('ccode');
+		echo "Code ID: ".$code;
+		$card = $this->MCardLoad->read_where(array('cardCode'=> $code));
+
+		if($card){
+			$card = json_decode(json_encode($card));
+			$u =  $this->MUser->read($this->session->userdata['userSession']->userID);
+			if($card[0]->cardStatus==1){
+				$cardNew = $u[0]->load_amt + $card[0]->cardAmount;
+				$res = $this->MUser->update($this->session->userdata["userSession"]->userID,array('load_amt'=>$cardNew));
+
+				if($res){
+					$code = $card[0]->cardId;
+					$res1 = $this->MCardLoad->update($code, array('cardStatus'=>0));
+				}
 			}
-		}else{
-			redirect("event/cEvent/viewEvents");
 		}
+
+		redirect("event/cEvent/viewEvents");
 	}
 	public function index()
 	{
@@ -50,9 +53,9 @@ class cUser extends CI_Controller {
 
 	public function signuppage()
 	{
-		
+
 		$this->load->view('user/vSignup.php');
-		
+
 	}
 
 
@@ -62,31 +65,36 @@ class cUser extends CI_Controller {
 
 		$now = NEW DateTime(NULL, new DateTimeZone('UTC'));
 
-		$data = array('user_name' => $this->input->post('uname'), 
+		$data = array('user_name' => $this->input->post('uname'),
 					  'password' => hash('sha512',$this->input->post('password')),
 					  'first_name' => $this->input->post('fname'),
 					  'last_name' => $this->input->post('lname'),
 					  'middle_initial' => $this->input->post('miname'),
 					  'email' => $this->input->post('email'),
 					  'birthdate' => $this->input->post('bdate'),
-					  'gender' => $this->input->post('gender'),					  
+					  'gender' => $this->input->post('gender'),
 					  'contact_no' => $this->input->post('contact'),
 					  'user_type' => 'Regular',
-					  'date_account_created' => $now->format('Y-m-d H:i:s')	
+					  'date_account_created' => $now->format('Y-m-d H:i:s')
 					);
-	
-		
+
+
 		$res = $this->MUser->read_where(array('user_name' => $data['user_name']));
 		$res1 = $this->MUser->read_where(array('email' => $data['email']));
 
     	if($res){
-    			redirect('user/cUser/viewSignUp');
+    			$this->session->set_flashdata('error_msg','Username taken');
+    			$this->data = $data;
+    			$this->viewSignUp();
+    			// redirect('user/cUser/viewSignUp',"refresh");
 				//echo "INVALID, EXISTING USERNAME, PLS TRY AGAIN";
 
 		}else if($res1){
-				redirect('user/cUser/viewSignUp');
+			$this->session->set_flashdata('error_msg','Email taken');
+			$this->data = $data;
+				$this->viewSignUp();
 				//echo "INVALID, EXISTING EMAIL, PLS TRY AGAIN";
-				
+
 		}else{
 
 			$result = $user->insert($data);
@@ -120,9 +128,9 @@ class cUser extends CI_Controller {
 
 	public function displayEventDetails($id)
 	{
-		
+
 		$uid = null;
-		
+
 		$data1 ['events']= $this->MEvent->loadEventDetails($id);
 		$gID = $this->MEvent->loadEventDetails($id);
 		foreach ($gID as $k) {
@@ -141,15 +149,33 @@ class cUser extends CI_Controller {
 
 	public function search(){
 		$data['events'] = $this->MEvent->getAllEvents();
-		$this->load->view('imports/vHeader');
+
+		$this->load->view('imports/vHeaderLandingPage');
+		//$this->load->view('imports/vHeader');
 		$this->load->view('user/vSearch.php');
 		// $this->load->view('user/vListEvents.php', $data);
 		$this->load->view('imports/vFooter');
 	}
 	public function viewSignUp()
 	{
+		if(!$this->data){
 		$this->load->view('imports/vHeaderSignUpPage');
 		$this->load->view('vSignUp');
 		$this->load->view('imports/vFooterLandingPage');
+		}else{
+			$this->load->view('imports/vHeaderSignUpPage');
+		$this->load->view('vSignUp',$this->data);
+		$this->load->view('imports/vFooterLandingPage');
+		}
+
+	}
+
+	public function viewAnnouncements()
+	{
+		$data['announcements'] = $this->MAnnouncement->loadAllAnnouncementDetails();
+		$this->load->view('imports/vHeader');
+		$this->load->view('user/vAnnouncementPage.php', $data);
+		$this->load->view('imports/vFooter');
+
 	}
 }
--- application/views/vLandingPage.php
+++ application/views/vLandingPage.php
@@ -4,31 +4,6 @@
         <div id="preloader">
             <div id="status">&nbsp;</div>
         </div>
-        <!-- Body content -->
-
-        <div class="header-connect">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-5 col-sm-8  col-xs-12">
-<!--                         <div class="header-half header-call">
-                            <p>
-                                <span><i class="pe-7s-call"></i> +1 234 567 7890</span>
-                                <span><i class="pe-7s-mail"></i> eventsystem@system.com</span>
-                            </p>
-                        </div> -->
-                    </div>
-                    <div class="col-md-2 col-md-offset-5  col-sm-3 col-sm-offset-1  col-xs-12">
-                        <div class="header-half header-social">
-                            <ul class="list-inline">
-                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>            
 
         <nav class="navbar navbar-default ">
             <div class="container">
@@ -49,14 +24,17 @@
                     </div>
 
                     <div class="button navbar-right">
-                        <button class="navbar-btn nav-button wow bounceInRight login"> <a href ="<?php echo site_url();?>/event/cEvent/viewCreateEvent" data-wow-delay="0.4s">Create Event </a></button>
+                        <a href ="<?php echo site_url();?>/event/cEvent/viewCreateEvent" data-wow-delay="0.4s"><button class="navbar-btn nav-button wow bounceInRight login"> Create Event </button></a>
                     </div>
-                    
+
 
                     <ul class="main-nav nav navbar-nav navbar-right">
                         <li class="wow fadeInDown" data-wow-delay="0.1s"><a href="<?php echo site_url();?>/cLogin/viewDashBoard">Home</a></li>
-                        
-                        <li class="wow fadeInDown" data-wow-delay="0.1s"><a href="<?php echo site_url();?>/event/cEvent/viewEvents">Profile</a></li><!-- 
+
+                        <li class="wow fadeInDown" data-wow-delay="0.1s"><a href="<?php echo site_url();?>/event/cEvent/viewEvents">Profile</a></li>
+                        <li class="wow fadeInDown" data-wow-delay="0.1s"><a href="<?php echo site_url();?>/user/cUser/viewAnnouncements">Announcements</a></li>
+                        <li class="wow fadeInDown" data-wow-delay="0.1s"><a href="<?php echo site_url();?>/finance/cCart/viewCart">View Cart</a></li>
+                        <!--
                         <li class="wow fadeInDown" data-wow-delay="0.1s"><a href ="<?php echo site_url();?>/event/cEvent/viewCreateEvent" >Contact</a></li>
                         <li class="wow fadeInDown" data-wow-delay="0.1s"><a href ="#" >Profile</a></li> -->
                     </ul>
@@ -71,9 +49,9 @@
             <div class="slider">
                 <div id="bg-slider" class="owl-carousel owl-theme">
 
-                    <div class="item"><img src= "<?php echo base_url('assets/nikkiAssets/img/slide1/slider-image-2.jpg')?>" alt="Mirror Edge"></div> 
-                    <div class="item"><img src= "<?php echo base_url('assets/nikkiAssets/img/slide1/slider-image-4.jpg')?>" alt="The Last of us"></div> 
-                    <div class="item"><img src= "<?php echo base_url('assets/nikkiAssets/img/slide1/slider-image-1.jpg')?>" alt="GTA V"></div>   
+                    <div class="item"><img src= "<?php echo base_url('assets/nikkiAssets/img/slide1/slider-image-2.jpg')?>" alt="Mirror Edge"></div>
+                    <div class="item"><img src= "<?php echo base_url('assets/nikkiAssets/img/slide1/slider-image-5.jpg')?>" alt="The Last of us"></div>
+                    <div class="item"><img src= "<?php echo base_url('assets/nikkiAssets/img/slide1/slider-image-3.jpg')?>" alt="GTA V"></div>
 
                 </div>
             </div>
@@ -81,33 +59,21 @@
                 <div class="row">
                     <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 col-sm-12">
                         <h2>See Events Near You</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi deserunt deleniti, ullam commodi sit ipsam laboriosam velit adipisci quibusdam aliquam teneturo!</p>
                         <div class="search-form wow pulse" data-wow-delay="0.8s">
 
-<<<<<<< HEAD
                             <form action="<?php echo site_url();?>/user/cEvent/searchEvent" class=" form-inline" method="POST">
                                 <span style="color: gray;">Search Event</span><span>aaa</span>
                                 <div class="form-group">
-                                    <input name="searchWord" type="text" class="form-control" placeholder="Key word">
+                                    <input name="searchWord" type="text" class="form-control" placeholder="Key word" pattern="[\sa-zA-z0-9]+">
                                 </div>
-                                <!-- <div class="form-group">                                     
+                                <!-- <div class="form-group">
                                     <select name="basic" class="form-control">
-=======
-                            <form action="" class=" form-inline">
-                                <span style="color: gray;">Search Event</span><span>aaa</span>
-                                <div class="form-group">
-                                    <input type="text" class="form-control" placeholder="Key word" pattern="[\sa-zA-z0-9]+">
-                                </div>
-                                <div class="form-group">                                     
-                                    <select id="basic" class="selectpicker show-tick form-control">
-                                        <option> -Category- </option>
->>>>>>> 2e2b5d715fb76f73cb9eaedb9483fe223541e46b
                                         <option>Concerts</option>
                                         <option>Workshops</option>
-                                        <option>Other</option>  
+                                        <option>Other</option>
                                     </select>
                                 </div> -->
-                                <button class="btn search-btn" type="submit"><i class="fa fa-search"></i></button> 
+                                <button class="btn search-btn" type="submit"><i class="fa fa-search"></i></button>
                             </form>
                         </div>
                     </div>
@@ -118,21 +84,21 @@
 
         <!-- property area -->
         <div class="content-area recent-property" style="padding-bottom: 60px; background-color: rgb(252, 252, 252);">
-            <div class="container">   
+            <div class="container">
                 <div class="row">
                     <div class="col-md-12  padding-top-40 properties-page">
-                        <div class="col-md-12 "> 
+                        <div class="col-md-12 ">
                             <div class="col-xs-10 page-subheader sorting pl0">
 
                                 <ul class="sort-by-list">
                                     <li class="active">
                                         <a href="javascript:void(0);" class="order_by_date" data-orderby="property_date" data-order="ASC">
-                                            Sort By Date <i class="fa fa-sort-amount-asc"></i>					
+                                            Sort By Date <i class="fa fa-sort-amount-asc"></i>
                                         </a>
                                     </li>
                                     <li class="">
                                         <a href="javascript:void(0);" class="order_by_price" data-orderby="property_price" data-order="DESC">
-                                            Sort By Price <i class="fa fa-sort-numeric-desc"></i>						
+                                            Sort By Price <i class="fa fa-sort-numeric-desc"></i>
                                         </a>
                                     </li>
                                 </ul>
@@ -156,13 +122,13 @@
 
                             <div class="col-xs-2 layout-switcher">
                                 <a class="layout-list" href="javascript:void(0);"> <i class="fa fa-th-list"></i>  </a>
-                                <a class="layout-grid active" href="javascript:void(0);"> <i class="fa fa-th"></i> </a>                          
+                                <a class="layout-grid active" href="javascript:void(0);"> <i class="fa fa-th"></i> </a>
                             </div><!--/ .layout-switcher-->
                         </div>
 
-                        <div class="col-md-12 "> 
+                        <div class="col-md-12 ">
                             <div id="list-type" class="proerty-th">
-                            <?php   
+                            <?php
                             $cnt =1;
                                         if(isset($events)){
                                             foreach ($events as $event) {
@@ -173,14 +139,13 @@
                                                     <a href="<?php echo site_url();?>/event/cEvent/displayEventDetails/<?php echo $event->event_id;?>"><img style="max-height: 70px;" src="<?php echo base_url();?>assets/dianeAssets/img/events<?php echo $cnt++; ?>.jpg"></a>
                                                 </div>
                                                    <div class="item-entry overflow">
-                                                        <h5><a href="<?php echo site_url();?>/cLogin/viewEventDetails"> <?php 
+                                                        <h5><a href="<?php echo site_url();?>/cLogin/viewEventDetails"> <?php
                                                     if(strlen($event->event_name)>=42){
-                                                        echo substr($event->event_name,0,39)."..."; 
+                                                        echo substr($event->event_name,0,39)."...";
                                                     }else{
                                                             echo $event->event_name;
                                                     }
                                                     ?></a></h5>
-<<<<<<< HEAD
                                                     <?php
                                                         echo $event->event_name;
 
@@ -192,21 +157,19 @@
 
                                                             if($now > $start && $now > $end){
                                                                 echo "<h5>Expired!</h5>";
-                                                            
+
                                                             }else if($now < $start){
                                                                 if($interval->days == 0){
                                                                     echo "<h5>Less than a day!</h5>";
                                                                 }else{
                                                                     echo "<h5>$interval->days day/s left!</h5>";
                                                                 }
-                                                                
+
                                                             }else if($now >= $start && $now <= $end){
                                                                 echo "<h5>Happening now!</h5>";
                                                             }
-                                                            
-                                                    ?>   
-=======
->>>>>>> 2e2b5d715fb76f73cb9eaedb9483fe223541e46b
+
+                                                    ?>
                                                         <div class="dot-hr"></div>
                                                         <!-- <span class="pull-left"><b> Date: </b> <?php echo $event->dateStart;?>  </span> -->
                                                         <span class="proerty-price pull-right"></span>
@@ -214,18 +177,18 @@
                                                         <!-- <div class="property-icon pull-right">
                                                             <a>Read More</a>
                                                         </div> -->
-                                                    </div> 
+                                                    </div>
                                             </div>
-                                        </div>    
+                                        </div>
                                     <?php
                                             }
                                         }
                                     ?>
-                                
+
 
                             </div>
                         </div>
-                        <div class="col-md-12"> 
+                        <div class="col-md-12">
                             <div class="pull-right">
                                 <div class="pagination">
                                     <ul>
@@ -237,15 +200,15 @@
                                         <li><a href="#">Next</a></li>
                                     </ul>
                                 </div>
-                            </div>                
+                            </div>
                         </div>
-                    </div>  
+                    </div>
                 </div>
             </div>
         </div>
 
 
-          <!-- Footer area-->
+        <!-- Footer area-->
         <div class="footer-area">
 
             <div class=" footer">
@@ -257,40 +220,21 @@
                                 <h4>About us </h4>
                                 <div class="footer-title-line"></div>
 
-                                <img src = "<?php echo base_url('assets/img/footer-logo.png')?>" class="wow pulse" data-wow-delay="1s">
+                               <img src= "<?php echo base_url('assets/dianeAssets/img/logoBlack.png')?>" alt="" class="wow pulse" data-wow-delay="1s" >
+                                <p>We help you reach out to the most interesting events anywhere they may be. The events you’ve always wanted to join and create will be in your hands with just a few clicks. Worry not because we’re here to help you discover the latest events this planet will ever have.</p>
 
-                                <p>Lorem ipsum dolor cum necessitatibus su quisquam molestias. Vel unde, blanditiis.</p>
-                                <ul class="footer-adress">
-                                    <li><i class="pe-7s-map-marker strong"> </i> 9089 your adress her</li>
-                                    <li><i class="pe-7s-mail strong"> </i> email@yourcompany.com</li>
-                                    <li><i class="pe-7s-call strong"> </i> +1 908 967 5906</li>
-                                </ul>
                             </div>
                         </div>
-                    
+
                         <div class="col-md-3 col-sm-6 wow fadeInRight animated">
                             <div class="single-footer news-letter">
                                 <h4>Contact Us</h4>
                                 <div class="footer-title-line"></div>
-                                <p>Lorem ipsum dolor sit amet, nulla  suscipit similique quisquam molestias. Vel unde, blanditiis.</p>
+                                <ul class="footer-adress">
+                                    <li><i class="pe-7s-mail strong"> </i> dailyEvents@gmail.com</li>
+                                    <li><i class="pe-7s-call strong"> </i> 253-2753</li>
+                                </ul>
 
-                                <form>
-                                    <div class="input-group">
-                                        <input class="form-control" type="text" placeholder="E-mail ... ">
-                                        <span class="input-group-btn">
-                                            <button class="btn btn-primary subscribe" type="button"><i class="pe-7s-paper-plane pe-2x"></i></button>
-                                        </span>
-                                    </div>
-                                    <!-- /input-group -->
-                                </form> 
-
-                                <div class="social pull-right"> 
-                                    <ul>
-                                        <li><a class="wow fadeInUp animated" href="#"><i class="fa fa-twitter"></i></a></li>
-                                        <li><a class="wow fadeInUp animated" href="#" data-wow-delay="0.2s"><i class="fa fa-facebook"></i></a></li>
-                                        <li><a class="wow fadeInUp animated" href="#" data-wow-delay="0.4s"><i class="fa fa-instagram"></i></a></li>
-                                    </ul> 
-                                </div>
                             </div>
                         </div>
 
@@ -302,23 +246,17 @@
                 <div class="container">
                     <div class="row">
                         <div class="pull-left">
-                            <span> (C) UI Module , All rights reserved 2017  </span> 
-                        </div> 
-                        <div class="bottom-menu pull-right"> 
-                            <ul> 
+                            <span> (C) UI Module , All rights reserved 2017  </span>
+                        </div>
+                        <div class="bottom-menu pull-right">
+                            <ul>
                                 <li><a class="wow fadeInUp animated" href="#" data-wow-delay="0.2s">Home</a></li>
                                 <li><a class="wow fadeInUp animated" href="#" data-wow-delay="0.3s">Events</a></li>
                                 <li><a class="wow fadeInUp animated" href="#" data-wow-delay="0.6s">Contact</a></li>
-                            </ul> 
+                            </ul>
                         </div>
                     </div>
                 </div>
             </div>
 
         </div>
-          
-
-
-    
-     
-
