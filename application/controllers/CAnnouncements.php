2f2f8a213fbcc54acf74d7d4360d414ababaf693
escoroiv <escoro.ivan@gmail.com>
2017-12-16 23:12:31 +0800

    Transfer all functions needed in your modules to certain controller

--- application/controllers/CAnnouncements.php
+++ application/controllers/CAnnouncements.php
@@ -16,7 +16,86 @@
 
 		/* FUNCTIONS RELATED TO USERS (PUT IT BELOW) */
 			/* ADMIN MODULE FUNCTIONS */
-				
+			//ANNOUNCEMENT FUNCTIONALITY - also added MAnnouncement in models/admin and autoload.php (12/04/17)
+			public function viewAnnouncements() {
+				$result_data=$this->readAllAdmin();
+				//////////////////////////////////////////////////////////////////////////////
+				//================INTERFACE MODULE - DATA-LAYOUT FILTERING CODE============//
+				/////////////////////////////////////////////////////////////////////////////
+				$array = array();
+				if($result_data){
+					foreach ($result_data as $value) {
+							$arrObj = new stdClass;
+							$arrObj->account_id = $value->account_id;
+							$arrObj->user_name = $value->user_name;
+							$arrObj->first_name = $value->first_name;
+							$arrObj->middle_initial = $value->middle_initial;
+							$arrObj->last_name = $value->last_name;
+							$arrObj->email= $value->email;
+							$arrObj->contact_no= $value->contact_no;
+							$arrObj->birthdate= $value->birthdate;
+							$arrObj->date_account_created = $value->date_account_created;
+							$arrObj->gender = $value->gender;
+							$arrObj->user_type = $value->user_type;
+							$arrObj->user_status = $value->user_status;
+							$arrObj->upgraded_by = $value->upgradedBy;//Added by admin module
+							$array[] = $arrObj;
+					}
+				}
+				////////////STOPS HERE///////////////////////////////////////////////////
+
+				$data2['admin']=$array;
+				$data2['ownAdminAccount']=$this->readOwnAdminAccount();
+				$data2['announcements']=$this->readAllAnnouncements();
+
+				$this->load->view('imports/admin_vHeader');
+				$this->load->view('admin/vAnnouncements', $data2);
+				$this->load->view('imports/admin_vFooter');
+			}
+			
+			public function createAnnouncement() {
+				$announcement = new MAnnouncement();
+
+				$now = NEW DateTime(NULL, new DateTimeZone('UTC'));
+
+				$data = array('announcementDetails' => $this->input->post('announcementDetails'),
+							  'announcementStatus' => 'OnGoing',
+							  'postedBy' => $this->input->post('postedBy'),
+							  'datePosted' => $now->format('Y-m-d H:i:s')
+							);
+
+				$result = $announcement->insert($data);
+
+				if($result){
+					//$this->index();
+					redirect('admin/cAdmin/viewAnnouncements');
+				}
+			}
+
+			// view all announcements
+			public function readAllAnnouncements(){
+				$result= $this->MAnnouncement->read_all();
+				if($result){
+					return $result;
+				}else{
+					return false;
+				}
+			}
+
+			public function deleteAnnouncement($id){
+				$announcement = new MAnnouncement();
+
+				$data = array('announcementID' => $id);
+				$results = $this->MAnnouncement->read_where($data);
+
+				if($results){
+					$response = $announcement->updateAnnouncementStatus($id, "Finished");
+
+					if ($response) {
+						redirect('admin/cAdmin/viewAnnouncements');
+					}
+				}
+			}
 
 			/* *************** */
 
--- application/controllers/CEvents.php
+++ application/controllers/CEvents.php
@@ -7,6 +7,14 @@
 			parent::__construct();
 		 	/* LOAD MODELS HERE */
 		 	//Ex: $this->load->model('MCalendar');
+			$this->load->model('user/MEvent');
+    	$this->load->model('user/MUser');
+    	$this->load->model('user/MTicketType');
+    	$this->load->model('user/MTicket');
+    	$this->load->helper('date');
+    	$this->load->model('MEventInfo');
+    	$this->error = "";
+    	$this->success = "";
 		}
 
 		public function index()
@@ -16,7 +24,274 @@
 
 		/* FUNCTIONS RELATED TO USERS (PUT IT BELOW) */
 			/* ADMIN MODULE FUNCTIONS */
-				
+			public function viewCreateFromCalendar(){
+
+
+		if(isset($_POST['startDate']) && isset($_POST['startTime']) && isset($_POST['endDate']) && isset($_POST['endTime']) ){
+			$data['start_date'] = $_POST['startDate'];
+			$data['start_time'] = $_POST['startTime'];
+			$data['end_date'] = $_POST['endDate'];
+			$data['end_time'] = $_POST['endTime'];
+			$result = $this->load->view('vEventForCalendar',$data,TRUE);
+			//$this->viewCreateEvent();
+			echo $result;
+		}
+
+
+
+	}
+
+	public function viewEditFromCalendar(){
+
+
+		$data1['start'] = $_POST['start'];
+		$data1['end'] = $_POST['end'];
+		$data1['title'] = $_POST['title'];
+		$data1['details'] = $_POST['details'];
+		$data1['category'] = $_POST['category'];
+		$data1['venue'] = $_POST['venue'];
+
+		$id = $_POST['id'];
+
+		$tid = $this->MEvent->joinEventTicketType($id);
+
+
+		$data2['ticket_info'] = null;
+		if($tid){
+			$data2['ticket_info'] = $tid;
+		}
+
+
+        $data = array_merge($data1, $data2);
+
+		$result = $this->load->view('vEditEvent',$data,TRUE);
+		//$this->viewCreateEvent();
+		echo $result;
+
+	}
+
+	public function viewCreateEvent()
+	{
+		$this->data['custom_js']= '<script type="text/javascript">
+                              	$("#user").addClass("active");
+                        </script>';
+
+        $this->load->view('imports/vHeaderSignUpPage');
+		$this->load->view('vNewEvent');
+		$this->load->view('imports/vFooterLandingPage');
+
+	}
+
+	public function viewEvents()
+	{
+		$userid = $this->session->userdata['userSession']->userID;
+		$result_data = $this->MEvent->getAllEventsByUser($userid);
+		//////////////////////////////////////////////////////////////////////////////
+		//================INTERFACE MODULE - DATA-LAYOUT FILTERING CODE============//
+		/////////////////////////////////////////////////////////////////////////////
+		$array = $array1 = $array2 = array();
+		if($result_data){
+			foreach ($result_data as $value) {
+					$arrObj = new stdClass;
+					$arrObj->event_id = $value->event_id;
+					$arrObj->event_name = $value->event_name;
+					$arrObj->dateStart = $value->dateStart;
+					$arrObj->dateEnd = $value->dateEnd;
+					$arrObj->event_details = $value->event_details;
+					$arrObj->event_status = $value->event_status;
+					$arrObj->event_venue = $value->event_venue;
+					$arrObj->event_category = $value->event_category;
+					$array[] = $arrObj;
+			}
+		}
+		////////////STOPS HERE///////////////////////////////////////////////////
+		$data['events']  = $array;
+
+		$result_data = $this->MUser->read($this->session->userdata['userSession']->userID);
+		//////////////////////////////////////////////////////////////////////////////
+		//================INTERFACE MODULE - DATA-LAYOUT FILTERING CODE============//
+		/////////////////////////////////////////////////////////////////////////////
+		if($result_data){
+			foreach ($result_data as $value) {
+					$arrObj = new stdClass;
+					$arrObj->load_amt = $value->load_amt;
+					$array1[] = $arrObj;
+			}
+		}
+		////////////STOPS HERE///////////////////////////////////////////////////
+		$data['user']   = $array1;
+
+		$result_data = $this->MEventInfo->getTransHistory($this->session->userdata['userSession']->userID);
+		//////////////////////////////////////////////////////////////////////////////
+		//================INTERFACE MODULE - DATA-LAYOUT FILTERING CODE============//
+		/////////////////////////////////////////////////////////////////////////////
+		if($result_data){
+			foreach ($result_data as $value) {
+					$arrObj = new stdClass;
+					$arrObj->Event = $value->Event;
+					$arrObj->Paid = $value->Paid;
+					$arrObj->DatePaid = $value->DatePaid;
+					$array2[] = $arrObj;
+			}
+
+		}
+		////////////STOPS HERE///////////////////////////////////////////////////
+		$data['hist']   = $array1;
+		$data['userid'] = $userid;
+
+		$this->load->view('imports/vHeaderLandingPage');
+		$this->load->view('vEvents',$data);
+		$this->load->view('imports/vFooterLandingPage');
+	}
+	public function displayEventDetails($id)
+	{
+
+		$uid = null; //to get organize name
+		$eid = null;
+
+		$result_data = $this->MEvent->loadEventDetails($id);
+		//////////////////////////////////////////////////////////////////////////////
+		//================INTERFACE MODULE - DATA-LAYOUT FILTERING CODE============//
+		/////////////////////////////////////////////////////////////////////////////
+		$array = $array1 = $array2 = $array3 = $array4 = array();
+		if($result_data){
+			foreach ($result_data as $value) {
+					$arrObj = new stdClass;
+					$arrObj->event_id = $value->event_id;
+					$arrObj->event_date_start = $value->event_date_start;
+					$arrObj->event_name = $value->event_name;
+					$arrObj->event_date_end = $value->event_date_end;
+					$arrObj->event_details = $value->event_details;
+					$arrObj->event_status = $value->event_status;
+					$arrObj->event_venue = $value->event_venue;
+					$arrObj->event_category = $value->event_category;
+					$arrObj->user_id = $value->user_id;
+					$array[] = $arrObj;
+			}
+		}
+		////////////STOPS HERE///////////////////////////////////////////////////
+		$data1 ['events']  = $array;
+
+		$gID = $this->MEvent->loadEventDetails($id);
+
+		foreach ($gID as $k) {
+			$eid = $k->event_id;
+			$uid = $k->user_id; //retrieve
+		}
+
+		// print_r($uid);
+		$result_data = $this->MUser->loadUserDetails($uid);
+		//////////////////////////////////////////////////////////////////////////////
+		//================INTERFACE MODULE - DATA-LAYOUT FILTERING CODE============//
+		/////////////////////////////////////////////////////////////////////////////
+		if($result_data){
+			foreach ($result_data as $value) {
+					$arrObj = new stdClass;
+					$arrObj->account_id = $value->account_id;
+					$arrObj->user_name = $value->user_name;
+					$arrObj->first_name = $value->first_name;
+					$arrObj->middle_initial = $value->middle_initial;
+					$arrObj->last_name = $value->last_name;
+					$arrObj->email= $value->email;
+					$arrObj->contact_no= $value->contact_no;
+					$arrObj->birthdate= $value->birthdate;
+					$arrObj->date_account_created = $value->date_account_created;
+					$arrObj->gender = $value->gender;
+					$arrObj->user_type = $value->user_type;
+					$arrObj->user_status = $value->user_status;
+					$arrObj->load_amt = $value->load_amt;
+					$array1[] = $arrObj;
+			}
+		}
+		////////////STOPS HERE////////////////////////////////////////////////////
+		$data2['organizer'] = $array1;
+
+		$result_data = $this->MTicketType->loadType($eid);
+		//////////////////////////////////////////////////////////////////////////////
+		//================INTERFACE MODULE - DATA-LAYOUT FILTERING CODE============//
+		/////////////////////////////////////////////////////////////////////////////
+		if($result_data){
+			foreach ($result_data as $value) {
+					$arrObj = new stdClass;
+					$arrObj->ticket_name = $value->ticket_name;
+					$arrObj->price = $value->price;
+					$arrObj->ticket_type_id = $value->ticket_type_id;
+					$array2[] = $arrObj;
+			}
+		}
+		////////////STOPS HERE////////////////////////////////////////////////////
+		$data3['types'] = $array2;
+
+		// $data4['tixStat'] = $this->MTicketType->getTicketStatus($eid);
+		// if(isset($data4['tixStat'])){
+		// 	$data = array_merge($data1,$data2,$data3,$data4);
+		// }else{
+
+		// }
+		$data = array_merge($data1,$data2,$data3);
+		$result_data= $this->MEvent->getGoingToEvent($id);
+		//////////////////////////////////////////////////////////////////////////////
+		//================INTERFACE MODULE - DATA-LAYOUT FILTERING CODE============//
+		/////////////////////////////////////////////////////////////////////////////
+		if($result_data){
+			foreach ($result_data as $value) {
+					$arrObj = new stdClass;
+					$arrObj->account_id = $value->account_id;
+					$arrObj->first_name = $value->first_name;
+					$arrObj->middle_initial = $value->middle_initial;
+					$arrObj->last_name = $value->last_name;
+					$array3[] = $arrObj;
+			}
+		}
+		////////////STOPS HERE////////////////////////////////////////////////////
+		$data['going'] = $array3;
+
+		$result_data = $this->MUser->read_where( array('account_id' =>$this->session->userdata['userSession']->userID  ));
+		//////////////////////////////////////////////////////////////////////////////
+		//================INTERFACE MODULE - DATA-LAYOUT FILTERING CODE============//
+		/////////////////////////////////////////////////////////////////////////////
+		if($result_data){
+			foreach ($result_data as $value) {
+					$arrObj = new stdClass;
+					$arrObj->account_id = $value->account_id;
+					$arrObj->user_name = $value->user_name;
+					$arrObj->first_name = $value->first_name;
+					$arrObj->middle_initial = $value->middle_initial;
+					$arrObj->last_name = $value->last_name;
+					$arrObj->email= $value->email;
+					$arrObj->contact_no= $value->contact_no;
+					$arrObj->birthdate= $value->birthdate;
+					$arrObj->date_account_created = $value->date_account_created;
+					$arrObj->gender = $value->gender;
+					$arrObj->user_type = $value->user_type;
+					$arrObj->user_status = $value->user_status;
+					$arrObj->load_amt = $value->load_amt;
+					$array4[] = $arrObj;
+			}
+		}
+		////////////STOPS HERE////////////////////////////////////////////////////
+		$data['user'] = $array4;
+		$data['id'] = $this->session->userdata['userSession']->userID;
+		if($this->error != ""){
+			$data['errorMsg']= $this->error;
+		 // print_r($data);
+		}
+
+		if($this->success != ""){
+			$data['successMsg']= $this->success;
+		 // print_r($data);
+		}
+
+		$this->load->view('imports/vHeaderLandingPage');
+		$this->load->view('vEventDetails',$data);
+		$this->load->view('imports/vFooterLandingPage');
+
+
+		// $this->load->view('imports/vHeader');
+		// $this->load->view('user/vEventRegistration', $data);
+		// $this->load->view('imports/vFooter');
+		# code...
+	}
 
 			/* *************** */
 
