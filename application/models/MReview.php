c256059370df183f1eaaca80274cffa3b4bab76e
Pancakes13 <neil.llenes@gmail.com>
2017-12-19 22:15:43 +0800

    Complete the controller function for displaying event reviews

--- application/models/MReview.php
+++ application/models/MReview.php
@@ -4,10 +4,36 @@
 		const DB_TABLE = "review";
         const DB_TABLE_PK = "review_id";
 
+        private $review_id;
         private $rating;
-        private $description
-        private $ticket_id
-            
+        private $description;
+        private $ticket_id;
+        
+        public function loadEventReviews($eventId)
+		{
+			$this->db->select('*');
+            $this->db->from("review as r");
+            $this->db->join("ticket as t","r.ticket_id = t.ticket_id");
+            $this->db->join("user_account as u","t.user_id = u.account_id");
+	        $this->db->join("ticket_type AS tt","t.ticket_type_id = t.ticket_type_id");
+	        $this->db->join("event_info AS e","e.event_id = tt.event_id");
+            $this->db->where("e.event_id = '$eventId'");
+            $this->db->group_by("r.review_id");
+
+			$query = $this->db->get();
+
+			return $query->result();
+			# code...
+		}
+
+        public function getReviewId(){
+            return $this->review_id;
+        }
+
+        public function setReviewId($review_id){
+            $this->review_id = $review_id;
+        }
+
         public function getRating(){
             return $this->Rating;
         }
