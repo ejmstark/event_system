63e7d4d601509589b31a97cfd96376fbf991a264
marykrisv <marykrisvill@gmail.com>
2017-12-21 00:55:40 +0800

    change again sa logging (event_db(7).sql) ang g sundan

--- application/controllers/CCard.php
+++ application/controllers/CCard.php
@@ -65,12 +65,12 @@ class cCard extends CI_Controller {
       $random = $this->randomCode();
     }while($this->isCodeUnique($random) != true);
 
-    $data = array ('card_id' => null,
-                   'card_code' => $random,
-                   'card_amount' => $amount,
-                   'card_active' => 1,
-                   'card_claimed' => null,
-                   'account_id' => $this->session->userdata["userSession"]->userID
+    $data = array ('cardId' => null,
+                   'cardCode' => $random,
+                   'cardAmount' => $amount,
+                   'cardStatus' => 1,
+                   'addedBy' => $this->session->userdata["userSession"]->userID,
+                   'updatedBy' => $this->session->userdata["userSession"]->userID
                   );
 
     $query = $card->insert($data);
