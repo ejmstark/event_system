c99ee35795dc53bdddde6490f96dca4409c72da0
jenadrianne <yan4414@gmail.com>
2018-01-14 17:11:56 +0800

    refix for #202

--- application/views/vLandingPage.php
+++ application/views/vLandingPage.php
@@ -15,7 +15,7 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="index-5.html"><img src="<?php echo base_url('assets/dianeAssets/img/logoBlack.png')?>"></a>
+                    <a class="navbar-brand" href="<?php echo site_url();?>/cLogin/viewDashBoard"><img src="<?php echo base_url('assets/dianeAssets/img/logoBlack.png')?>"></a>
                 </div>
 
                 <div class="collapse navbar-collapse yamm" id="navigation">
