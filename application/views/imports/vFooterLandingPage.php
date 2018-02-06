9391c04609820d30ade1f4514996237178020e21
Christian Ted Ochoa <ochoatechmaster25@gmail.com>
2018-02-06 20:32:02 +0800

    Fixed Issue #346

--- application/views/imports/vFooterLandingPage.php
+++ application/views/imports/vFooterLandingPage.php
@@ -1,19 +1,19 @@
         <script src="<?php echo base_url('assets/nikkiAssets/js/modernizr-2.6.2.min.js')?>" rel="stylesheet" /> </script>
         <script src="<?php echo base_url('assets/nikkiAssets/js/jquery-1.10.2.min.js')?>" rel="stylesheet" /> </script>
         <script src="<?php echo base_url('assets/nikkiAssets/bootstrap/js/bootstrap.min.js')?>" rel="stylesheet" /> </script>
-       
+
         <script src="<?php echo base_url('assets/nikkiAssets/js/bootstrap-select.min.js')?>" rel="stylesheet" /> </script>
         <script src="<?php echo base_url('assets/nikkiAssets/js/bootstrap-hover-dropdown.js')?>" rel="stylesheet" /> </script>
         <script src="<?php echo base_url('assets/nikkiAssets/js/easypiechart.min.js')?>" rel="stylesheet" /> </script>
         <script src="<?php echo base_url('assets/nikkiAssets/js/owl.carousel.min.js')?>" rel="stylesheet" /> </script>
-        
+
         <script src="<?php echo base_url('assets/nikkiAssets/js/wow.js')?>" rel="stylesheet" /> </script>
         <script src="<?php echo base_url('assets/nikkiAssets/js/icheck.min.js')?>" rel="stylesheet" /> </script>
         <script src="<?php echo base_url('assets/nikkiAssets/js/price-range.js')?>" rel="stylesheet" /> </script>
         <script src="<?php echo base_url('assets/nikkiAssets/js/main.js')?>" rel="stylesheet" /> </script>
 
         <script  src="<?php echo base_url(); ?>assets/jsKyleAssets/moment.min.js"></script>
-        <script src="<?php echo base_url('assets/jsKyleAssets/bootstrap.min.js')?>" rel="stylesheet" /> </script>
+        <!-- <script src="<?php echo base_url('assets/jsKyleAssets/bootstrap.min.js')?>" rel="stylesheet" /> </script> -->
         <script src="<?php echo base_url('assets/jsKyleAssets/fullcalendar.js')?>" rel="stylesheet" /> </script>
 
         <script src="<?php echo base_url('assets/dianeAssets/js/bootstrap-notify.min.js')?>" rel="stylesheet" /> </script>
