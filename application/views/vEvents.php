a3852fa8ed91eae3ac0f6abc400162a1ab186921
jenadrianne <yan4414@gmail.com>
2018-02-07 18:25:07 +0800

    enhancements

--- application/views/vEvents.php
+++ application/views/vEvents.php
@@ -271,12 +271,7 @@
   <div class="tab-content">
     <div role="tabpanel" class="tab-pane active" id="home">
         <div class="col-md-12 clear">
-            <div class="row">
-                <div class="col-xs-12 layout-switcher pull-right">
-                    <a class="layout-list" href="javascript:void(0);"> <i class="fa fa-th-list"></i>  </a>
-                    <a class="layout-grid active" href="javascript:void(0);"> <i class="fa fa-th"></i> </a>
-                </div>
-            </div>
+           
 
             <div id="list-type" class="proerty-th">
 
